e40634c1ee7d198af8f6c2fa9d60a402c0527aa7
Eugene Sylenko <1863154+ffenix113@users.noreply.github.com>
2024-05-01 19:56:37 +0200

    chore: add build info logging
    This will provide some information about build env and version

--- cmd/zigbee/firmware/root.go
+++ cmd/zigbee/firmware/root.go
@@ -1,6 +1,13 @@
 package firmware
 
-import "github.com/urfave/cli/v2"
+import (
+	"errors"
+	"log"
+	"runtime/debug"
+	"slices"
+
+	"github.com/urfave/cli/v2"
+)
 
 func RootCmd() *cli.Command {
 	return &cli.Command{
@@ -15,6 +22,38 @@ func RootCmd() *cli.Command {
 				Name:  "workdir",
 				Usage: "change the working directory for the build process (currently does not do anything)",
 			},
+		},
+		Before: func(ctx *cli.Context) error {
+			buildInfo, ok := debug.ReadBuildInfo()
+			if !ok {
+				return errors.New("could not read build information")
+			}
+
+			vcsProps := make([]debug.BuildSetting, 0, 2)
+
+			for _, setting := range buildInfo.Settings {
+				if !slices.Contains([]string{"vcs.revision", "vcs.modified"}, setting.Key) {
+					continue
+				}
+
+				vcsProps = append(vcsProps, setting)
+				if len(vcsProps) == 3 {
+					break
+				}
+			}
+
+			slices.SortFunc(vcsProps, func(a, b debug.BuildSetting) int {
+				if a.Key > b.Key {
+					return 1
+				}
+
+				return -1
+			})
+
+			// Information that will help with investigation of issues
+			log.Printf("build info: %s/%s, %s", buildInfo.GoVersion, buildInfo.Main.Version, vcsProps)
+
+			return nil
 		},
 	}
 }
