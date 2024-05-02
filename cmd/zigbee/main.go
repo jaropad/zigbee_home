b4663895f4c3d37dc338e18c67d077222730978e
Eugene Sylenko <1863154+ffenix113@users.noreply.github.com>
2024-05-02 08:39:57 +0200

    chore: move version printing to separate flag
    Now version is printable through `--version/-v` flag at the root.

--- cmd/zigbee/main.go
+++ cmd/zigbee/main.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"context"
+	"errors"
 	"log"
 	"os"
+	"runtime/debug"
+	"slices"
 
 	"github.com/ffenix113/zigbee_home/cmd/zigbee/firmware"
 	"github.com/urfave/cli/v2"
@@ -23,6 +26,19 @@ func main() {
 				Name:  "config",
 				Value: "zigbee.yaml",
 			},
+			&cli.BoolFlag{
+				Name:    "version",
+				Aliases: []string{"v"},
+			},
+		},
+		Action: func(ctx *cli.Context) error {
+			if ctx.Bool("version") {
+				return printVersion()
+			}
+
+			cli.ShowAppHelpAndExit(ctx, 0)
+
+			return nil
 		},
 	}
 
@@ -30,4 +46,37 @@ func main() {
 		log.Println(err.Error())
 		os.Exit(1)
 	}
+}
+
+func printVersion() error {
+	buildInfo, ok := debug.ReadBuildInfo()
+	if !ok {
+		return errors.New("could not read build information")
+	}
+
+	vcsProps := make([]debug.BuildSetting, 0, 2)
+
+	for _, setting := range buildInfo.Settings {
+		if !slices.Contains([]string{"vcs.revision", "vcs.modified"}, setting.Key) {
+			continue
+		}
+
+		vcsProps = append(vcsProps, setting)
+		if len(vcsProps) == cap(vcsProps) {
+			break
+		}
+	}
+
+	slices.SortFunc(vcsProps, func(a, b debug.BuildSetting) int {
+		if a.Key > b.Key {
+			return 1
+		}
+
+		return -1
+	})
+
+	// Information that will help with investigation of
+	log.Printf("%s, tag:%s, version:%s", buildInfo.GoVersion, buildInfo.Main.Version, vcsProps)
+
+	return nil
 }
